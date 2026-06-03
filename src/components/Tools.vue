42554be48414f05b1dd7b44639b4ff7a1ac090e2
hbramores <hbramores@gmail.com>
2026-06-03 23:21:36 +0800

    updated tools

--- src/components/Tools.vue
+++ src/components/Tools.vue
@@ -10,22 +10,44 @@
 				<div class="tool-card h-100 text-center p-4">
 					<h2 class="mb-4">Front End</h2>
 					<div class="tools-icons d-flex flex-wrap justify-content-center gap-3">
-						<img src="/images/tools/html5.png" alt="HTML5">
-						<img src="/images/tools/css3.png" alt="CSS3">
-						<img src="/images/tools/Boots.png" alt="Bootstrap">
-						<img src="/images/tools/javascript.png" alt="JavaScript">
+						<img src="/images/tools/html5.png" alt="HTML5" title="HTML5">
+						<img src="/images/tools/css3.png" alt="CSS3" title="CSS3">
+						<img src="/images/tools/Boots.png" alt="Bootstrap" title="Bootstrap">
+						<img src="/images/tools/javascript.png" alt="JavaScript" title="JavaScript">
 					</div>
 				</div>
 			</div>
 
 			<div class="col-12 col-md-6 col-lg-4">
 				<div class="tool-card h-100 text-center p-4">
-					<h2 class="mb-4">MERN</h2>
+					<h2 class="mb-4">MERN & MEVN</h2>
 					<div class="tools-icons d-flex flex-wrap justify-content-center gap-3">
-						<img src="/images/tools/mongodb.png" alt="MongoDB">
-						<img src="/images/tools/express.png" alt="Express">
-						<img src="/images/tools/react.png" alt="React">
-						<img src="/images/tools/node.png" alt="Node.js">
+						<img src="/images/tools/mongodb.png" alt="MongoDB" title="MongoDB">
+						<img src="/images/tools/express.png" alt="Express" title="Express">
+						<img src="/images/tools/react.png" alt="React" title="React">
+						<img src="/images/tools/vue.png" alt="Vue.js" title="Vue.js">
+						<img src="/images/tools/node.png" alt="Node.js" title="Node.js">
+					</div>
+				</div>
+			</div>
+
+			<div class="col-12 col-md-6 col-lg-4">
+				<div class="tool-card h-100 text-center p-4">
+					<h2 class="mb-4">Databases</h2>
+					<div class="tools-icons d-flex flex-wrap justify-content-center gap-3">
+						<img :src="'/images/tools/sql.png'" alt="SQL" title="SQL">
+						<img :src="'/images/tools/mysql.png'" alt="MySQL" title="MySQL">
+					</div>
+				</div>
+			</div>
+
+			<div class="col-12 col-md-6 col-lg-4">
+				<div class="tool-card h-100 text-center p-4">
+					<h2 class="mb-4">Programming & UI</h2>
+					<div class="tools-icons d-flex flex-wrap justify-content-center gap-3">
+						<img :src="'/images/tools/java.png'" alt="Java" title="Java">
+						<img :src="'/images/tools/javafx.png'" alt="JavaFX" title="JavaFX">
+						<img :src="'/images/tools/scene-builder.png'" alt="Scene Builder" title="Scene Builder">
 					</div>
 				</div>
 			</div>
@@ -34,15 +56,19 @@
 				<div class="tool-card h-100 text-center p-4">
 					<h2 class="mb-4">Other Tools</h2>
 					<div class="tools-icons d-flex flex-wrap justify-content-center gap-3">
-						<img src="/images/tools/heroku.png" alt="Heroku">
-						<img src="/images/tools/git.png" alt="Git">
-						<img src="/images/tools/postman.png" alt="Postman">
-						<img src="/images/tools/sublime.png" alt="Sublime Text">
+						<img src="/images/tools/heroku.png" alt="Heroku" title="Heroku">
+						<img src="/images/tools/git.png" alt="Git" title="Git">
+						<img :src="'/images/tools/github.png'" alt="GitHub" title="GitHub">
+						<img :src="'/images/tools/visual-studio.png'" alt="Visual Studio" title="Visual Studio">
+						<img :src="'/images/tools/vercel.png'" alt="Vercel" title="Vercel">
+						<img :src="'/images/tools/render.jpg'" alt="Render" title="Render">
+						<img src="/images/tools/postman.png" alt="Postman" title="Postman">
+						<img src="/images/tools/sublime.png" alt="Sublime Text" title="Sublime Text">
 
-						<img src="/images/tools/photoshop.png" alt="photoshop">
-						<img src="/images/tools/canva.png" alt="Canva">
-						<img src="/images/tools/capcut.png" alt="Capcut">
-						<img src="/images/tools/notion.png" alt="Notion">
+						<img src="/images/tools/photoshop.png" alt="Photoshop" title="Photoshop">
+						<img src="/images/tools/canva.png" alt="Canva" title="Canva">
+						<img src="/images/tools/capcut.png" alt="CapCut" title="CapCut">
+						<img src="/images/tools/notion.png" alt="Notion" title="Notion">
 
 					</div>
 				</div>
